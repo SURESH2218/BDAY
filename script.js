73ce04f4b4eafd3812645765054654b58d09477f
SURESH2218 <94789102+SURESH2218@users.noreply.github.com>
2021-11-21 18:32:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "girishma" && b=="22-11-2001")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
